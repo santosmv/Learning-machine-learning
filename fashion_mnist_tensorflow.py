62192a22a89df6c190987b61fc571d94cae5b247
mvsantos001 <mvsantos@ifi.unicamp.br>
2021-08-14 10:00:12 -0300

    Update keras note

--- fashion_mnist_tensorflow.py
+++ fashion_mnist_tensorflow.py
@@ -3,33 +3,38 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+#take the dataset
 fashion_mnist = keras.datasets.fashion_mnist
-
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+#creating a model to optimize
 model = keras.Sequential([
-    #28: number of pixels of each image
+    #28: number of pixels of each image (entries)
     keras.layers.Flatten(input_shape=(28,28)),
     #128: number of functions to be optimized
     keras.layers.Dense(128, activation=tf.nn.relu),
-    #10: nmber of fashion items
+    #10: number of fashion items
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
+#compiling the model
 model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy')
 
-model.fit(train_images, train_labels, epochs=1)
+#fit the model to data
+model.fit(train_images, train_labels, epochs=3)
 
+#results of the evaluation
 results = model.evaluate(test_images, test_labels)
 
 predictions = model.predict(test_images)
-print(predictions[0])
+#print(predictions)
 
-predict_number = np.argmax(predictions[0])
-print(predict_number)
+for i in range(len(test_labels)):
+    predict_number = np.argmax(predictions[i])
+    print('valor verdadeiro =', test_labels[i], 'predição =', predict_number)
 
 plt.figure()
 plt.imshow(test_images[0])
 plt.colorbar()
-plt.grid(False)
+plt.grid(True)
 plt.show()
